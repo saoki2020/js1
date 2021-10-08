72c092a5f473a10bd9987c0a064383f81631d486
saoki2020 <md.mendelevium101@gmail.com>
2021-10-08 10:06:29 +0900

    changed function literal to arrow function

--- js/js1_3.js
+++ js/js1_3.js
@@ -1,16 +1,16 @@
 'use strict';
 {
-  let task_list = ["掃除", "買い物", "散歩"];
+  const task_list = ["掃除", "買い物", "散歩"];
 
-  function show_task() {
-  console.log("========================");
-  console.log("現在持っているタスク一覧");
-  console.log("========================");
+  let show_task = () => {
+    console.log("========================");
+    console.log("現在持っているタスク一覧");
+    console.log("========================");
 
-  task_list.forEach(function(value, index) {
-    console.log(`${index} : ${value}`);
-  });
-}
+    task_list.forEach((value, index) => {
+      console.log(`${index} : ${value}`);
+    });
+  }
 
   show_task();
   const input_task = prompt("タスクを入力してください");
@@ -20,6 +20,6 @@
     task_list.push(input_task);
     show_task();
   }
-  
+
   prompt("「確認,追加,削除,終了」の4つのいずれかを入力してください");
 }
